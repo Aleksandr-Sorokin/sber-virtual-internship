62aa771812fbf09217de549f4e341f1a9b884fd4
Aleksandr <al-sorokin.yd@yandex.ru>
2023-03-05 17:45:39 +0700

    Add | findMaxPopulationInCity

--- src/main/java/ru/sorokin/sbervirtualinternship/SberVirtualInternshipApplication.java
+++ src/main/java/ru/sorokin/sbervirtualinternship/SberVirtualInternshipApplication.java
@@ -1,28 +1,15 @@
 package ru.sorokin.sbervirtualinternship;
 
-import ru.sorokin.sbervirtualinternship.enums.TypeSortCity;
-import ru.sorokin.sbervirtualinternship.model.City;
 import ru.sorokin.sbervirtualinternship.service.CityService;
 
 import java.io.File;
-import java.util.List;
 
 public class SberVirtualInternshipApplication {
 
     public static void main(String[] args) {
-        TypeSortCity typeSort = TypeSortCity.DESC_NAME_CASE_INSENSITIVE;
-        if (args.length > 0) {
-            if (TypeSortCity.DESC_NAME_CASE_INSENSITIVE.equals(TypeSortCity.valueOf(args[0])) ||
-                    TypeSortCity.DESC_DISTRICT_CASE_SENSITIVE.equals(TypeSortCity.valueOf(args[0]))) {
-                typeSort = TypeSortCity.valueOf(args[0]);
-            }
-        }
 
         File file = new File("Задача ВС Java Сбер.csv");
         CityService cityService = new CityService();
-        List<City> cities = cityService.createListCity(file, typeSort);
-        for (City city : cities) {
-            System.out.println(city);
-        }
+        System.out.println(cityService.findMaxPopulationInCity(file));
     }
 }
--- src/main/java/ru/sorokin/sbervirtualinternship/service/CityService.java
+++ src/main/java/ru/sorokin/sbervirtualinternship/service/CityService.java
@@ -14,7 +14,8 @@ public class CityService {
     public CityService() {
         this.readFiles = new ReadFiles();
     }
-    public List<City> createListCity(File file, TypeSortCity sort) {
+
+    public List<City> createSortListCity(File file, TypeSortCity sort) {
         Comparator<City> compareCity = Comparator.comparing(City::getId);
         if (TypeSortCity.DESC_DISTRICT_CASE_SENSITIVE.equals(sort)) {
             compareCity = Comparator.comparing(City::getDistrict).thenComparing(City::getName);
@@ -36,5 +37,36 @@ public class CityService {
             }
         }).sorted(compareCity).collect(Collectors.toList());
         return cities;
+    }
+
+    public String findMaxPopulationInCity(File file) {
+        int count = 0;
+        int maxPopulationIndex = 0;
+        int maxPopulation = 0;
+        List<String> stringList = readFiles.getLinesFromFile(file);
+        City[] cities = new City[stringList.size()];
+        for (String city : stringList) {
+            String[] cityToArray = city.split(";");
+            if (Integer.valueOf(cityToArray[4]) > maxPopulation) {
+                maxPopulation = Integer.valueOf(cityToArray[4]);
+                maxPopulationIndex = count;
+            }
+            if (cityToArray.length < 6) {
+                cities[count++] = new City(Integer.valueOf(cityToArray[0]),
+                        cityToArray[1],
+                        cityToArray[2],
+                        cityToArray[3],
+                        Integer.valueOf(cityToArray[4]),
+                        "");
+            } else {
+                cities[count++] = new City(Integer.valueOf(cityToArray[0]),
+                        cityToArray[1],
+                        cityToArray[2],
+                        cityToArray[3],
+                        Integer.valueOf(cityToArray[4]),
+                        cityToArray[5]);
+            }
+        }
+        return String.format("[%d] = %d", maxPopulationIndex, maxPopulation);
     }
 }
